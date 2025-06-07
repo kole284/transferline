66880bc29067ef12546a5166c4f505a611f13bfc
kole284 <nikolampek04@gmail.com>
2025-06-08 01:12:34 +0200

    fixed deployment errors

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,5 @@
 import Hero from '../../components/hero/Hero.tsx'
 import Hero2 from '../../components/hero2/Hero2.tsx'
-import Hero3 from '../../components/hero3/Hero3.tsx'
-import Hero4 from '../../components/hero4/Hero4.tsx'
 function Home(){
   return(
     <>
